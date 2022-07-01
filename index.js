f42564c90219c056be98cfae39fc8ddb275543fa
Nischal Bade <nischal.bade.13@gmail.com>
2022-07-01 08:43:02 +0545

    Deploy To heroku

--- index.js
+++ index.js
@@ -81,26 +81,20 @@ app.post('/api/persons',(request,response,next) => {
     }
 })
 
-app.put('api/persons',(request,response,next) => {
+app.put('/api/persons/:id',(request,response,next) => {
   
-  if (request.body.name === undefined || request.body.number === undefined) {
-    
+  if (request.body.id === undefined || request.body.number === undefined) {
     response.status(400).json({ error: 'Missing fields in request' })
   } else {
-  const person ={
-    number: request.body.number
-  }
-  const { content, important } = request.body
-  Person.findByIdAndUpdate(request.params.id, 
-    { name, number },
-    { new: true, runValidators: true, context: 'query' }
-    )
-    .then(updatedPerson => {
-      response.json(updatedPerson.toJSON())
-      
-    })
-    .catch(error => next(error))
+    const person = {
+        number: request.body.number
+    }
 
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
   }
 })
 
@@ -108,9 +102,9 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+      return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+      return response.status(400).send({ error: 'validation error' })
   }
 
   next(error)
--- models/phonebook.js
+++ models/phonebook.js
@@ -22,9 +22,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type:String,
-        minLength: 5,
-        required: true
-    }
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+              return /\d{2,3}-\d{4}.*/.test(v);
+            },
+        }
+    }   
 })
 
 personSchema.set('toJSON', {
